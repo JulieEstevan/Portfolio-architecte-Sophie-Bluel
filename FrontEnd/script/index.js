569ddac02a03493a40bcf715ba4d47ecf5ce6f0e
JulieEstevan <julie.s.estevan@gmail.com>
2024-02-12 10:37:00 +0100

    [update] optimisation et correction de la recuperation des donnees API et de l'affichage dynamique des travaux

--- FrontEnd/script/index.js
+++ FrontEnd/script/index.js
@@ -1,16 +1,32 @@
-const response = await fetch("http://localhost:5678/api/works")
-const works = await response.json()
-const worksGallery = document.querySelector(".gallery")
-let i
-
-for (i = 0; i < works.length; i++) {
-    const workContainer = document.createElement("figure")
-    const workImage = document.createElement("img")
-    workImage.src = works[i].imageUrl
-    const workTitle = document.createElement("figcaption")
-    workTitle.innerHTML = works[i].title
-
-    worksGallery.appendChild(workContainer)
-    workContainer.appendChild(workImage)
-    workContainer.appendChild(workTitle)
+const fetchWorks = async () => {
+    const responseWorks = await fetch("http://localhost:5678/api/works")
+    const works = responseWorks.json()
+    return works
 }
+const works = await fetchWorks()
+console.log("works", works)
+
+const fetchCategories = async () => {
+    const responseCategories = await fetch("http://localhost:5678/api/categories")
+    const categories = responseCategories.json()
+    return categories
+}
+const categories = await fetchCategories()
+console.log("categories", categories)
+
+const displayWorks = () => {
+    works.forEach(works => {
+        const worksGallery = document.querySelector(".gallery")
+        const workContainer = document.createElement("figure")
+        const workImage = document.createElement("img")
+        workImage.src = works.imageUrl
+        const workTitle = document.createElement("figcaption")
+        workTitle.innerHTML = works.title
+
+        worksGallery.appendChild(workContainer)
+        workContainer.appendChild(workImage)
+        workContainer.appendChild(workTitle)
+    });
+}
+
+displayWorks()
